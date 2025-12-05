b81f4e0c94cfe65d0b5e7a9a95b15e321b5c368d
sar0868 <safarov-ar@yandex.ru>
2025-12-05 14:20:50 +0300

    create branch 4-interface

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -5,13 +5,59 @@ import (
 	"fmt"
 )
 
-func main() {
-	binList := bins.NewBinList()
-	id := "0001"
-	private := false
-	name := "first item"
-	bin1 := bins.NewBin(id, private, name)
-	binList.Bins = append(binList.Bins, *bin1)
+// Меню - создать бин, найти бины по id-private-name, посмотреть все бины, выход
 
-	fmt.Println(binList)
+var menu = map[string]func(*bins.BinList){
+	"1": addBin,
+	"2": findBins,
+	"3": printBins,
 }
+
+func main() {
+	bins := bins.NewBinList()
+
+	fmt.Println("Bins")
+Menu:
+	for {
+		choice := promptData(
+			"1: add bin",
+			"2: find bins",
+			"3: print bins",
+			"4: exit",
+			"Input choice",
+		)
+		menuFunc := menu[choice]
+		if menuFunc == nil {
+			break Menu
+		}
+		menuFunc(bins)
+	}
+
+	// binList := bins.NewBinList()
+	// id := "0001"
+	// private := false
+	// name := "first item"
+	// bin1 := bins.NewBin(id, private, name)
+	// binList.Bins = append(binList.Bins, *bin1)
+
+	// fmt.Println(binList)
+}
+
+func promptData(prompt ...string) string {
+	for i, el := range prompt {
+		if i == len(prompt)-1 {
+			fmt.Printf("%v: ", el)
+		} else {
+			fmt.Println(el)
+		}
+	}
+	var choice string
+	fmt.Scan(&choice)
+	return choice
+}
+
+func addBin(bins *bins.BinList) {}
+
+func findBins(bins *bins.BinList) {}
+
+func printBins(bins *bins.BinList) {}
